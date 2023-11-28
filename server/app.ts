51ad74b085eaf58208decc3dc68421a54fc00b37
bon <kaste2233@gmail.com>
2023-11-28 16:27:37 +0900

    add prettier, eslint settings

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express, { Response } from 'express';
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import tweetsRoute from './router/tweet';
@@ -10,20 +10,20 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan('tiny'));
 app.use(
-  cors({
-    origin: ['http://localhost:3000'],
-  })
+	cors({
+		origin: ['http://localhost:3000'],
+	})
 );
 
 app.use('/tweets', tweetsRoute);
 
-app.use((req, res, next) => {
-  res.sendStatus(404);
+app.use((req, res) => {
+	res.sendStatus(404);
 });
 
-app.use((error: any, req: any, res: Response<any, any>) => {
-  console.error(error);
-  res.sendStatus(500);
+app.use((error: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+	console.error(error);
+	res.sendStatus(500);
 });
 
 app.listen(8080);
---
+++ server/readme.md
@@ -0,0 +1,3 @@
+# ts 설정 참고 : https://www.bezkoder.com/express-typescript-example/#Express
+
+# prettier, eslint 설정 참고 : https://velog.io/@str2023/express-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EC%83%9D%EC%84%B1%EC%8B%9C-eslint-prettier-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0
