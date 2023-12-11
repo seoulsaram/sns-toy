58bbb80d78d4656c4998de5d35ca2aa10b583b73
bon <kaste2233@gmail.com>
2023-12-11 15:33:28 +0900

    [client] 불필요한 콘솔로그 삭제

--- client/src/util/findAgent.ts
+++ client/src/util/findAgent.ts
@@ -1,11 +1,9 @@
 export const isMobile = () => {
 	const user = navigator.userAgent;
-	console.log('userAgent', user);
 	let isCheck = false;
 
 	if (user.indexOf('iPhone') > -1 || user.indexOf('Android') > -1) {
 		isCheck = true;
 	}
-	console.log('isCheck', isCheck);
 	return isCheck;
 };
